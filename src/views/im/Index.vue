80ab01d7cfa33ead8e95afbfb0a0e3765fbc6393
liukaiwen <18404974906@163.com>
2022-04-07 18:57:32 +0800

    vue3模板ref

--- src/views/im/Index.vue
+++ src/views/im/Index.vue
@@ -1,25 +1,38 @@
-<script setup>
-import { ref, onMounted } from 'vue'
+<script lang="ts" setup>
+// import axios from 'axios'
+// import { ref, watch, watchEffect, watchPostEffect, onMounted } from 'vue'
+import Container from '@/components/Container.vue'
 
-import children_ from './children_.vue'
+// // 初始化
+// const x = ref(0)
+// const y = ref(0)
+// const z = ref('init')
 
-const list = ref([
-  1111,
-  22,333333333333,44
-])
+// let _watch = watch(x, (nv, ov) => {
+//   console.log(nv, ov)
+// })
 
-const _children_ = ref(null)
+// const list = ref([12,26,32,49,51])
+// const itemsRef = ref([])
 
-onMounted(() => {
-  console.log(_children_.value)
-})
+// onMounted(() => {
+//   console.log(itemRef.value.map(item => {
+//     console.log(item)
+//   }))
+// })
 </script>
 
 <template>
-  <children_ ref="_children_"></children_>
-  <ul>
-    <li v-for="item in list" ref="itemRefs">
-      {{ item }}
-    </li>
-  </ul>
+<!-- <button @click="_watch()">停止监听</button> -->
+  <!-- <input v-model="x" type="number"> -->
+  <!-- <input v-model="y" type="number"> -->
+  <!-- {{ z }} -->
+  <!-- <li v-for="item in list" ref="itemsRef">{{ item }}</li> -->
+  <Container :noData="true"></Container>
 </template>
+
+<style scoped lang="less">
+.im{
+  display: flex;
+}
+</style>
